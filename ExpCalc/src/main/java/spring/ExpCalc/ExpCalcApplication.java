c75cd2582931c9e0483eb50f59f5b75c70ba8693
Raphael Getachew <rapha@Raphaels-MacBook-Pro.local>
2023-02-06 19:04:04 -0500

    initial commit

---
+++ ExpCalc/src/main/java/spring/ExpCalc/ExpCalcApplication.java
@@ -0,0 +1,13 @@
+package spring.ExpCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpCalcApplication.class, args);
+	}
+
+}
---
+++ ExpCalc/src/main/java/spring/ExpCalc/WebController.java
@@ -0,0 +1,36 @@
+package spring.ExpCalc;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class WebController {
+
+	// @RequestMapping("/exp")
+	// public Maple Exp(@RequestParam(defaultValue = "1") int stlvl, @RequestParam(defaultValue = "200") int endlvl, @RequestParam(defaultValue = "NONE") String type) {
+	// 	Maple response = new Maple();
+	// 	response.setexp();
+	// 	return response;
+	// }
+	@RequestMapping(path = "/exp/{stlvl}/{endlvl}/{type}", method = RequestMethod.GET)
+	public Maple Exp(@PathVariable int stlvl, @PathVariable int endlvl, @PathVariable String type) {
+		Maple response = new Maple();
+		response.setstlvl(stlvl);
+		response.setendlvl(endlvl);
+		response.settype(type);
+		response.setexp();
+		return response;
+	}
+
+	@RequestMapping(path = "/exp/{stlvl}/{endlvl}", method = RequestMethod.GET)
+	public Maple Exp(@PathVariable int stlvl, @PathVariable int endlvl) {
+		Maple response = new Maple();
+		response.setstlvl(stlvl);
+		response.setendlvl(endlvl);
+		response.settype("NONE");
+		response.setexp();
+		return response;
+	}
+}
